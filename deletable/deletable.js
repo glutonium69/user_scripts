ff0fbbf54f33108406f59e6b1d6415893ca35f9a
glutonium69 <146271709+glutonium69@users.noreply.github.com>
2024-10-07 22:30:06 +0600

    made code a little bit cleaner and added comments

--- deletable/deletable.js
+++ deletable/deletable.js
@@ -2,7 +2,7 @@
 // @name         Deletable
 // @namespace    http://tampermonkey.net/
 // @version      2024-10-07
-// @description  Lets you delete any elements or add them back after deletion on any page
+// @description  try to take over the world!
 // @author       You
 // @match        *://*/*
 // @icon         *://*/*
@@ -14,8 +14,16 @@
 
     console.log("running deletable");
 
-    let deletionBuffer = [];
-    let deletionSiblingParentBuffer = [];
+    /*
+    buffer contains data of the deleted element
+    array items look at follows
+      {
+          deleted: e.target, // the deleted element itself
+          parent: e.target.parentNode, // its parent
+          nextSibling: e.target.nextSibling // its next sibling
+      }
+    */
+    const deletionBuffer = [];
 
     const pressed = {
       d: false,
@@ -37,26 +45,26 @@
 
       if (pressed.d) {
         e.preventDefault();
-        deletionBuffer.push(e.target);
 
-        deletionSiblingParentBuffer.push({
+        deletionBuffer.push({
+          deleted: e.target,
           parent: e.target.parentNode,
-          sibling: e.target.nextSibling
+          nextSibling: e.target.nextSibling // next sibling returns null if targeted element is the last child node
         });
 
         e.target.remove();
       }
 
       if (pressed.e) {
-        if (deletionBuffer.length > 0 && deletionSiblingParentBuffer.length > 0) {
-          if (deletionSiblingParentBuffer.at(-1).sibling === null) {
-            deletionSiblingParentBuffer.at(-1).parent.append(deletionBuffer.at(-1));
-          } else {
-            deletionSiblingParentBuffer.at(-1).parent.insertBefore(deletionBuffer.at(-1), deletionSiblingParentBuffer.at(-1).sibling);
-          }
-          deletionBuffer.pop();
-          deletionSiblingParentBuffer.pop();
+        if (deletionBuffer.length === 0) return;
+
+        if (deletionBuffer.at(-1).nextSibling === null) { // means it was the last element and had no next sibling
+          deletionBuffer.at(-1).parent.append(deletionBuffer.at(-1).deleted); // just append to the parent
+        } else { // does have a next sibling
+          deletionBuffer.at(-1).parent.insertBefore(deletionBuffer.at(-1).deleted, deletionBuffer.at(-1).nextSibling); // insert before the sibling
         }
+
+        deletionBuffer.pop();
       }
     });
 
